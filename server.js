9e470dc44cab7846dc8a6163757b06a35dbeffd0
Kymaeda <kyo.m.720@gmail.com>
2023-11-15 08:34:15 +0900

    ユーザ新規登録APIの実装
    ref. https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/querying-the-database-typescript-postgresql
    
    - @prisma/clientのインストールが必要(migrateした時に一緒にインストールされたので、本コミットには含まれない)
    - 以下を参考に、userに対するcreate処理を記載する
      - https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/querying-the-database-typescript-postgresql#write-data-into-the-database
    - passwordのハッシュかはbycryptを使う(後続タスクで実装)
      - https://www.npmjs.com/package/bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const express = require('express')
-const app = express()
-const PORT = 3001
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const app = express();
+const PORT = 3001;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const prisma = PrismaClient();
+
+// 新規ユーザ登録API
+app.post('/api/auth/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // TODO: passwordのハッシュ化
+  const user = await prisma.user.create({ data: { name, email, password } });
+
+  return res.json(user);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
