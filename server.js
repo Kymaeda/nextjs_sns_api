d55dc66f56f30c78edaa9930f5e21cdb553e6779
Kymaeda <kyo.m.720@gmail.com>
2023-11-16 08:38:02 +0900

    fix typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const saltRounds = 10;
 const PORT = 3001;
 
-const prisma = PrismaClient();
+const prisma = new PrismaClient();
 
 // 新規ユーザ登録API
 app.post('/api/auth/register', async (req, res) => {
